78d1f78e00defb31eb6547115b1987cf888a5c3d
Roman <gachko@gmail.com>
2021-11-02 21:42:18 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// // let i = 100;
+// // while (i >= 50) {
+// //   console.log( i );
+// //   i--;
+// }
+
+// let i = 40;
+// while (i >= 0) {
+//   console.log( i );
+//   i--;
+// }
+
+
+
+// let myArray = [];
+
+// for (let i = 0; i < 10; i++) { 
+//     myArray.push('x');
+// }
+
+// console.log(myArray);
+
+
+let myArray = [];
+
+for (let i = 0; i < 3; i++) { 
+    myArray.push(prompt());
+}
+
+console.log(myArray);
